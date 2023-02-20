f280ac934cae0ddf31a0b86224f8e2bfd73b1c59
Adewale Fatoki <adewaleftk@gmail.com>
2023-02-20 20:39:18 +0100

    Day 5

---
+++ contracts/modifiers.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
+pragma solidity ^0.8.0;
+
+contract ExampleModifier {
+    address public owner;
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;  // this indicates that the rest of the function should be executed
+    }
+    
+    function doSomething() public onlyOwner {
+        // Only the owner of the contract can execute this function
+    }
+}
